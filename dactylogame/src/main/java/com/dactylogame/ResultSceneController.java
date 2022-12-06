bb0fc114d3c4a4f5a8851370be9a879ae28d574b
diamant <diamant.alexandre@gmail.com>
2022-12-06 12:56:53 +0100

    correction

--- dactylogame/src/main/java/com/dactylogame/ResultSceneController.java
+++ dactylogame/src/main/java/com/dactylogame/ResultSceneController.java
@@ -1,15 +1,8 @@
 package com.dactylogame;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.text.DecimalFormat;
 
-import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
